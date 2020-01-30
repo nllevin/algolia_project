ef4f1cafb10c4f64e7ef77f862bae0818066fbcb
nllevin <50681582+nllevin@users.noreply.github.com>
2020-01-30 15:19:57 -0500

    Updates

---
+++ precache-manifest.459b38d377e06d7e02f1e7f036d4e6e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02da64df4216491a7b60",
+    "url": "/static/css/main.4fb3358c.chunk.css"
+  },
+  {
+    "revision": "02da64df4216491a7b60",
+    "url": "/static/js/main.02da64df.chunk.js"
+  },
+  {
+    "revision": "c733c94755d9bd8d35ac",
+    "url": "/static/js/1.c733c947.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "423aa8ab6efbf6bd6c2a93d05d61d1c2",
+    "url": "/index.html"
+  }
+];
