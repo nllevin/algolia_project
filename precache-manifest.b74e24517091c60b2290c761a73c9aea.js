a1cac87682f0075a33325c8452d82199b78e74eb
nllevin <50681582+nllevin@users.noreply.github.com>
2020-01-30 15:41:28 -0500

    Updates

---
+++ precache-manifest.b74e24517091c60b2290c761a73c9aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02da64df4216491a7b60",
+    "url": "/algolia_project/static/css/main.4fb3358c.chunk.css"
+  },
+  {
+    "revision": "02da64df4216491a7b60",
+    "url": "/algolia_project/static/js/main.02da64df.chunk.js"
+  },
+  {
+    "revision": "b60f549a08e84fbc3625",
+    "url": "/algolia_project/static/js/1.b60f549a.chunk.js"
+  },
+  {
+    "revision": "1a9dd551e85d5b3d67a8",
+    "url": "/algolia_project/static/js/runtime~main.1a9dd551.js"
+  },
+  {
+    "revision": "848e36b1d04fbf1d9c011c9f34b8efdb",
+    "url": "/algolia_project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.459b38d377e06d7e02f1e7f036d4e6e6.js"
+  "/algolia_project/precache-manifest.b74e24517091c60b2290c761a73c9aea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/algolia_project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
