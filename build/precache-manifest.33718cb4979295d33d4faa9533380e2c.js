dfe552cdc9a57caaebefc0472d38f083ad322dc6
nllevin <50681582+nllevin@users.noreply.github.com>
2020-01-30 15:53:39 -0500

    Modify project's file structure for gh-pages deployment

---
+++ build/precache-manifest.33718cb4979295d33d4faa9533380e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39e11b5ccc887a6b1060",
+    "url": "/algolia_project/static/css/main.e4661fb5.chunk.css"
+  },
+  {
+    "revision": "39e11b5ccc887a6b1060",
+    "url": "/algolia_project/static/js/main.39e11b5c.chunk.js"
+  },
+  {
+    "revision": "b60f549a08e84fbc3625",
+    "url": "/algolia_project/static/js/1.b60f549a.chunk.js"
+  },
+  {
+    "revision": "1a9dd551e85d5b3d67a8",
+    "url": "/algolia_project/static/js/runtime~main.1a9dd551.js"
+  },
+  {
+    "revision": "2f6c2b777f1170051eb9ddc9ba09518c",
+    "url": "/algolia_project/index.html"
+  }
+];
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  GOOGLE_MAPS_API_KEY: "AIzaSyCqYyn09MIAAlC0N-ahrTaAaMnTqzUbYIw"
+};
